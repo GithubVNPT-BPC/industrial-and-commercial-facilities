f7a3b923b8e8ab188b2533107010d668df78430c
Quan Bui <quandxbp@gmail.com>
2021-02-02 10:33:27 +0700

    [FILTER] add filter services and filter for search partners

---
+++ src/app/_services/filter.service.ts
@@ -0,0 +1,55 @@
+import { Injectable } from "@angular/core";
+import { normalizeValue , unicodeToAZ} from "src/app/_services/stringUtils.service";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FilterService {
+    public filterValues = {};
+
+    public createFilter() {
+        let filterFunction = function (data: any, filter: string): boolean {
+            let searchTerms = JSON.parse(filter);
+            let isFilterSet = false;
+            for (const col in searchTerms) {
+                if (searchTerms[col].toString() !== '') {
+                    isFilterSet = true;
+                } else {
+                    delete searchTerms[col];
+                }
+            }
+    
+            let nameSearch = () => {
+                let found = false;
+                if (isFilterSet) {
+                    for (const col in searchTerms) {
+                        searchTerms[col].trim().toLowerCase().split(' ').forEach(word => {
+                            let value = data[col].toString();
+                            if (unicodeToAZ(normalizeValue(value)).indexOf(word) != -1 && isFilterSet) {
+                                found = true
+                            }
+                        });
+                    }
+                    return found
+                } else {
+                    return true;
+                }
+            }
+            return nameSearch()
+        }
+        return filterFunction
+    };
+
+    public getFilters(col, value) {
+        let val = unicodeToAZ(normalizeValue(value));
+        this.filterValues[col] = val;
+        return JSON.stringify(this.filterValues);
+    }
+
+    public clearFilter(obj, resetFields) {
+        for (let field in resetFields) {
+            obj[resetFields[field]] = "";
+        }
+        this.filterValues = {};
+    }
+}
