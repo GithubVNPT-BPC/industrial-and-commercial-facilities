da0930654fa8ac7cc8482b8eb58fec540524ac07
= <=>
2021-05-05 17:00:22 +0700

    phap_vo_doi

--- src/app/components/specialized/enery-management/future-electrical-plan/future-electrical-plan.component.ts
+++ src/app/components/specialized/enery-management/future-electrical-plan/future-electrical-plan.component.ts
@@ -30,7 +30,7 @@ export class FutureElectricalPlanComponent extends BaseComponent {
         { id_trang_thai_hoat_dong: 2, ten_trang_thai_hoat_dong: 'Ngừng hoạt động' }
     ];
 
-    loai_quy_hoach = {
+    loai_quy_hoach_2 = {
         1: 'Trạm biến áp 110KV',
         2: 'Trạm biến áp 220KV',
         3: 'Trạm biến áp 500KV',
@@ -38,6 +38,18 @@ export class FutureElectricalPlanComponent extends BaseComponent {
         5: 'Đường dây 220KV',
         6: 'Đường dây 500KV',
     }
+    loai_quy_hoach = {
+        1: 'Trạm biến áp 110KV',
+        2: 'Trạm biến áp 220KV',
+        3: 'Trạm biến áp 500KV',
+    }
+
+    loai_quy_hoach1 = {
+        4: 'Đường dây 110KV',
+        5: 'Đường dây 220KV',
+        6: 'Đường dây 500KV',
+    }
+
     selectedType = "1";
     authorize: boolean = true
 
