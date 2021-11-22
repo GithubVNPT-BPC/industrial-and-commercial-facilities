5c15b7a7bb5659c3516935e29a560eeacdd6873d
Welcome <Welcome@DESKTOP-U60D086>
2021-11-22 09:52:04 +0700

    commit on 'main'

--- src/app/components/specialized/industry-management/industrial-explosives/industrial-explosives.component.ts
+++ src/app/components/specialized/industry-management/industrial-explosives/industrial-explosives.component.ts
@@ -71,6 +71,7 @@ export class IndustrialExplosivesComponent extends BaseComponent {
         if (this._login.userValue.user_role_id == 5 || this._login.userValue.user_role_id == 1) {
             this.authorize = false
         }
+        this.filterModel.is_expired = false
     }
 
     ngAfterViewInit() {
