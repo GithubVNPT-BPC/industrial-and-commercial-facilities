f4e30e41b0a8db0653b3e88ec7e4dc0c086a0b34
toumeielegy <binhdo26.k42@st.ueh.edu.vn>
2021-12-04 10:55:09 +0700

    04/12/2021 binhdtr.bpc
    Sửa lỗi component report

--- src/app/components/report/report.routing.ts
+++ src/app/components/report/report.routing.ts
@@ -1,29 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {FillReportComponent} from '../report/fill-report/fill-detail/fill-detail.component';
-import {FillSelectReportComponent} from '../report/fill-report/fill-select-report/fill-select-report.component';
-import {ViewReportComponent} from '../report/view-report/view-detail/view-detail.component';
-import {ViewSelectReportComponent} from '../report/view-report/select-report/select-report.component';
-import {SummaryReportComponent} from '../report/summary-report/summary-report.component';
-import {LrisIntegrationComponent} from '../report/lris-integration/lris-integration.component';
+import { FillReportComponent } from '../report/fill-report/fill-detail/fill-detail.component';
+import { FillSelectReportComponent } from '../report/fill-report/fill-select-report/fill-select-report.component';
+import { ViewReportComponent } from '../report/view-report/view-detail/view-detail.component';
+import { ViewSelectReportComponent } from '../report/view-report/select-report/select-report.component';
+import { SummaryReportComponent } from '../report/summary-report/summary-report.component';
+import { LrisIntegrationComponent } from '../report/lris-integration/lris-integration.component';
 import { StandardReportComponent } from './standard-report/standard-report.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: StandardReportComponent,
     data: {
       title: 'Báo cáo'
     },
-    children: [
-      {
-        path: 'all',
-        component: FillSelectReportComponent,
-        data: {
-          title: 'Nhập báo cáo'
-        }
-      },
+    children: [{
+      path: '',
+      component: StandardReportComponent,
+      data: {
+        title: 'Báo cáo'
+      }
+    },
+    {
+      path: 'summary',
+      component: SummaryReportComponent,
+      data: {
+        title: 'Tổng hợp báo cáo'
+      }
+    },
+    {
+      path: 'lris',
+      component: LrisIntegrationComponent,
+      data: {
+        title: 'Báo cáo Kinh tế - Xã hội'
+      }
+    }
+      // {
+      //   path: 'all',
+      //   component: FillSelectReportComponent,
+      //   data: {
+      //     title: 'Nhập báo cáo'
+      //   }
+      // },
       // {
       //   path: 'edit',
       //   component: FillReportComponent,
@@ -45,20 +64,6 @@ const routes: Routes = [
       //     title: 'Xem báo cáo'
       //   }
       // },
-      {
-        path: 'summary',
-        component: SummaryReportComponent,
-        data: {
-          title: 'Tổng hợp báo cáo'
-        }
-      },
-      {
-        path: 'lris',
-        component: LrisIntegrationComponent,
-        data: {
-          title: 'Báo cáo Kinh tế - Xã hội'
-        }
-      },
     ]
   }
 ];
