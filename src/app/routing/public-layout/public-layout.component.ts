eba0a2451e18554da381c40115300c9d3848af95
Quan Bui <quandxbp@gmail.com>
2022-02-18 09:27:24 +0700

    [IMP] responsive navbar

--- src/app/routing/public-layout/public-layout.component.ts
+++ src/app/routing/public-layout/public-layout.component.ts
@@ -1,9 +1,11 @@
 import { ViewportScroller } from '@angular/common';
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, Input } from '@angular/core';
 import { NavigationEnd } from '@angular/router';
 import { onMainContentChange } from 'src/app/_animations/animation-sidebar';
 import { MODULE_CONTROL } from 'src/app/_enums/module-control.enum';
 import { SidebarService } from 'src/app/_services/sidebar.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { MatSidenav } from '@angular/material';
 
 @Component({
   selector: 'app-public-layout',
@@ -17,7 +19,13 @@ export class PublicLayoutComponent implements OnInit {
 
   public pageYoffset = 0;
   public onSideNavChange: boolean;
-  constructor(public _sidenavService: SidebarService, public scrollTop: ViewportScroller) {
+  private _opened = true;
+  @Input() sidebar: MatSidenav;
+
+  constructor(
+    public _sidenavService: SidebarService, 
+    public scrollTop: ViewportScroller,
+    private observer: BreakpointObserver) {
     this._sidenavService.sideBarState$.subscribe(res => {
       this.onSideNavChange = res;
     })
@@ -25,6 +33,12 @@ export class PublicLayoutComponent implements OnInit {
 
   ngOnInit() {
     window.addEventListener('scroll', this.scrollEvent, true);
+  }
+
+  ngAfterViewInit() {
+    this.observer.observe([`(max-width: 800px)`]).subscribe((res) => {
+      this._opened = !res.matches
+    });
   }
 
   numberOfClicks = 0;
