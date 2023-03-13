9709bf20c264923d9c5229dc6afa4b9f40a344cb
Welcome <Welcome@DESKTOP-U60D086>
2023-03-13 15:50:06 +0700

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,9 @@
 
 export const environment = {
   production: false,
-  // apiEndpoint: 'http://apicongthongtin.vnptbinhphuoc.vn/',
-  apiEndpoint: 'http://localhost:54961/',
+  apiEndpoint: 'http://apicongthongtin.vnptbinhphuoc.vn/',
+  // apiEndpoint: 'http://10.77.160.66:82/',
+  // apiEndpoint: 'http://localhost:54961/',
   // apiEndpoint: 'http://10.77.129.40:8080/',
   // apiEndpoint: 'http://192.168.110.66:9091/'
 };
