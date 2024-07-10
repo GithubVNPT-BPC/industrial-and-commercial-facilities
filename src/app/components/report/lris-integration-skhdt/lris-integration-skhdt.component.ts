bdb57d63ca8035086450148784ca5393f94ca5fd
Khoa Nguyen <khoana.bpc@gmail.com>
2024-07-10 16:13:14 +0700

    update

--- src/app/components/report/lris-integration-skhdt/lris-integration-skhdt.component.ts
+++ src/app/components/report/lris-integration-skhdt/lris-integration-skhdt.component.ts
@@ -18,6 +18,7 @@ import { ConfirmationDialogService } from 'src/app/shared/confirmation-dialog/co
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatSort } from '@angular/material/sort';
 import { bcskhdt_thang } from "src/app/_models/APIModel/report.model";
+import { BehaviorSubject } from "rxjs";
 
 const moment = _rollupMoment || _moment;
 export const MY_FORMATS = {
@@ -65,9 +66,9 @@ export class LrisIntegrationSkhdtComponent implements OnInit {
     // 'indicator',
     'indicatorname',
     'indunit',
-    'th_nam_truoc','kh_nam','th_thang_truoc','gc_th_thang_truoc','th_thang','gc_th_thang','luy_ke',
-    'ck_th_thang','ck_gc_th_thang','ck_luy_ke',
-    'ss_th_thang_sck','ss_th_thang_skh','ss_luy_ke_sck','ss_luy_ke_skh',
+    'th_nam_truoc', 'kh_nam', 'th_thang_truoc', 'gc_th_thang_truoc', 'th_thang', 'gc_th_thang', 'luy_ke',
+    'ck_th_thang', 'ck_gc_th_thang', 'ck_luy_ke',
+    'ss_th_thang_sck', 'ss_th_thang_skh', 'ss_luy_ke_sck', 'ss_luy_ke_skh',
   ];
 
   dataSource: MatTableDataSource<bcskhdt_thang> = new MatTableDataSource();
@@ -81,11 +82,6 @@ export class LrisIntegrationSkhdtComponent implements OnInit {
   public timechange: number = parseInt(_moment().format('YYYYMM'));
   public month: string
 
-  private exportRecord = {};
-  private importRecord = {};
-  private exportIndicator = 'CTDHTT_04';
-  private importIndicator = 'CTDHTT_05';
-
   access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiaW5ocGh1b2NAMTIzIiwibmFtZSI6ImJpbmhwaHVvYy5zeW5jIiwiaWF0IjoxNTE2MjM5MDEyMywidGVuYW50X2lkIjozOX0.0Znt0dLSvETuB1KimtqrjZQlT26N9EppaJnIQaSBXB0";
 
   selectedReport = 0;
@@ -93,6 +89,10 @@ export class LrisIntegrationSkhdtComponent implements OnInit {
     id: 0,
     name: 'Báo cáo các chỉ tiêu Kinh tế Xã hội SKHDT | Thực hiện Tháng'
   }]
+
+  inputdata = new BehaviorSubject<Array<bcskhdt_thang>>(null);
+  sctdata = new BehaviorSubject<[]>(null);
+  planningdata = new BehaviorSubject<any[]>(null);
 
   constructor(
     public sctService: SCTService,
@@ -122,14 +122,6 @@ export class LrisIntegrationSkhdtComponent implements OnInit {
   getReportName() {
     let self = this;
     return this.reportList.find(x => x.id == self.selectedReport).name;
-  }
-
-  getDataFromSystem() {
-    this.importRecord = { ...this.getDefaultValues() };
-    this.exportRecord = { ...this.getDefaultValues() };
-    this.getImportData(this.timechange);
-    this.getExportData(this.timechange);
-    // this.getPlanningData();
   }
 
   getCurrentMonthData() {
@@ -181,6 +173,58 @@ export class LrisIntegrationSkhdtComponent implements OnInit {
           this.paginator._intl.previousPageLabel = "Trang Trước";
           this.paginator._intl.nextPageLabel = "Trang Tiếp";
         } else {
+          this.dataSource = new MatTableDataSource<bcskhdt_thang>([]);
+          this.logger.msgError(result['err_msg']);
+        }
+      } else {
+        this.dataSource = new MatTableDataSource<bcskhdt_thang>([]);
+        this.logger.msgError("Lỗi khi lấy dữ liệu từ phần mềm Kinh tế xã hội");
+      }
+    });
+  }
+
+  getInputData() {
+    let payload = {
+      "func": "getReport",
+      "data": {
+        "header": {
+          "code": "SKHVDT_KTXH_THT",
+          "org": "70.SO_CT",
+          "period": this.timechange.toString()
+        }
+      },
+      "access_token": this.access_token
+    }
+    this.sctService.GetLrisReportData(payload).subscribe((result) => {
+      if (result) {
+        if (result['err_code'] == "0") {
+          let data = result['data']['data'];
+          let bcdata = new Array<bcskhdt_thang>();
+          data.forEach(s => {
+            let object = {
+              idx: s.idx,
+              indicator: s.indicator,
+              indicatorname: s.indicatorname,
+              indunit: s.indunit,
+              th_nam_truoc: s.value[0],
+              kh_nam: s.value[1],
+              th_thang_truoc: s.value[2],
+              gc_th_thang_truoc: s.value[3],
+              th_thang: s.value[4],
+              gc_th_thang: s.value[5],
+              luy_ke: s.value[6],
+              ck_th_thang: s.value[7],
+              ck_gc_th_thang: s.value[8],
+              ck_luy_ke: s.value[9],
+              ss_th_thang_sck: s.value[10],
+              ss_th_thang_skh: s.value[11],
+              ss_luy_ke_sck: s.value[12],
+              ss_luy_ke_skh: s.value[13],
+            }
+            bcdata.push(object)
+          });
+          this.inputdata.next(bcdata)
+        } else {
           this.logger.msgError(result['err_msg']);
         }
       } else {
@@ -194,20 +238,32 @@ export class LrisIntegrationSkhdtComponent implements OnInit {
       "func": "getReport",
       "data": {
         "header": {
-          "code": "BPC_SXKD_KHN",
+          "code": "SKHVDT_KTXH_KHN",
           "org": "70.SO_CT",
-          "period": this.theYear.toString()
+          "period": this.timechange.toString().substring(0, 4)
         }
       },
       "access_token": this.access_token
     }
     this.sctService.GetLrisReportData(payload).subscribe((result) => {
       if (result) {
-        let datas = result['data']['data'];
-        let filterImportRecord = datas.find(x => x.indicator == 'KHDT_SXKD_146');
-        let filterExportRecord = datas.find(x => x.indicator == 'KHDT_SXKD_130');
-        this.importRecord['ke_hoach_nam'] = filterImportRecord ? filterImportRecord['value'][1] : 0;
-        this.exportRecord['ke_hoach_nam'] = filterExportRecord ? filterExportRecord['value'][1] : 0;
+        if (result['err_code'] == "0") {
+          let data = result['data']['data'];
+          let bcdata = []
+          data.forEach(s => {
+            let object = {
+              idx: s.idx,
+              indicator: s.indicator,
+              indicatorname: s.indicatorname,
+              indunit: s.indunit,
+              kh_nam: s.value[1],
+            }
+            bcdata.push(object)
+          });
+          this.planningdata.next(bcdata)
+        } else {
+          this.logger.msgError(result['err_msg']);
+        }
       } else {
         this.logger.msgError("Lỗi khi lấy dữ liệu từ phần mềm Kinh tế xã hội");
       }
@@ -215,91 +271,70 @@ export class LrisIntegrationSkhdtComponent implements OnInit {
   }
 
   getDataFromLris() {
-    this.importRecord = { ...this.getDefaultValues() };
-    this.exportRecord = { ...this.getDefaultValues() };
     this.getCurrentMonthData();
-    // this.getPlanningData();
+    this.getInputData();
+    this.getPlanningData();
     this.getThucHienThang(this.timechange);
   }
 
-  getImportData(time_id: number) {
-    this.sctService.GetDanhSachNhapKhau(time_id).subscribe((result) => {
-      this.importRecord['thuc_hien_thang'] = result.data[0] && result.data[0].length ? result.data[0][0].tri_gia_thang || 0 : 0;
-    });
-  }
-
-  getExportData(time_id: number) {
-    this.sctService.GetDanhSachXuatKhau(time_id).subscribe((result) => {
-      this.exportRecord['thuc_hien_thang'] = result.data[0] && result.data[0].length ? result.data[0][0].tri_gia_thang || 0 : 0;
-    });
-  }
-
-  getThucHienThang(time_id: number){
+  getThucHienThang(time_id: number) {
     this.sctService.GetMonthlyReportSKHDT(time_id).subscribe((result) => {
-      console.log(result);
+      this.sctdata.next(result.data)
     });
-  }
-
-  getDefaultValues() {
-    return {
-      'thuc_hien_thang': 0,
-      'thuc_hien_thang_truoc': 0,
-      'luy_ke': 0,
-      'ke_hoach_nam': 0,
-      'thuc_hien_thang_cung_ky': 0,
-      'luy_ke_cung_ky': 0,
-      'thuc_hien_thang_so_voi_cung_ky': 0,
-      'thuc_hien_so_voi_ke_hoach': 0,
-      'luy_ke_so_voi_cung_ky': 0,
-      'luy_ke_so_voi_ke_hoach': 0,
-    }
   }
 
   sendData() {
+    let inputdata_t = []
+    let sctdata_t = []
+    let planningdata_t = []
+    inputdata_t = this.inputdata.value
+    sctdata_t = this.sctdata.value
+    planningdata_t = this.planningdata.value
+    let input = []
+
+    if (sctdata_t.length < 60) {
+      this.logger.msgError("Dữ liệu tổng hợp SCT chưa đầy đủ!");
+      return
+    } else {
+      inputdata_t.forEach(function (s, i) {
+        let obj = {
+          "Indicator": s.indicator,
+          "Value": [
+            s.th_nam_truoc ? parseFloat(s.th_nam_truoc) : null,
+            planningdata_t[i].kh_nam ? parseFloat(planningdata_t[i].kh_nam) : null,
+            s.th_thang_truoc ? parseFloat(s.th_thang_truoc) : null,
+            s.gc_th_thang_truoc ? parseFloat(s.gc_th_thang_truoc) : null,
+            sctdata_t[i].thuc_hien_thang ? parseFloat(sctdata_t[i].thuc_hien_thang) : null,
+            null,
+            s.luy_ke ? parseFloat(s.luy_ke) + (sctdata_t[i].thuc_hien_thang ? parseFloat(sctdata_t[i].thuc_hien_thang) : 0) : null,
+            s.ck_th_thang ? parseFloat(s.ck_th_thang) : null,
+            s.ck_gc_th_thang ? parseFloat(s.ck_gc_th_thang) : null,
+            s.ck_luy_ke ? parseFloat(s.ck_luy_ke) : null,
+            s.ck_th_thang ? (sctdata_t[i].thuc_hien_thang ? parseFloat(sctdata_t[i].thuc_hien_thang) : 0) / (s.ck_th_thang ? parseFloat(s.ck_th_thang) : 0) * 100 : null,
+            planningdata_t[i].kh_nam ? (sctdata_t[i].thuc_hien_thang ? parseFloat(sctdata_t[i].thuc_hien_thang) : 0) / (planningdata_t[i].kh_nam ? parseFloat(planningdata_t[i].kh_nam) : 0) * 100 : null,
+            s.ck_luy_ke ? (parseFloat(s.luy_ke) + (sctdata_t[i].thuc_hien_thang ? parseFloat(sctdata_t[i].thuc_hien_thang) : 0)) / (s.ck_luy_ke ? parseFloat(s.ck_luy_ke) : 0) * 100 : null,
+            planningdata_t[i].kh_nam ? (parseFloat(s.luy_ke) + (sctdata_t[i].thuc_hien_thang ? parseFloat(sctdata_t[i].thuc_hien_thang) : 0)) / (planningdata_t[i].kh_nam ? parseFloat(planningdata_t[i].kh_nam) : 0) * 100 : null,
+          ]
+        }
+        input.push(obj)
+      })
+    }
+
     let payload = {
       "func": "sndData",
       "data": {
         "Header": {
-          "Code": "CTDHTT_THANG",
+          "Code": "SKHVDT_KTXH_THT",
           "Org": "70.SO_CT",
           "Period": this.timechange.toString()
         },
-        "Data": [{
-          "Indicator": this.exportIndicator,
-          "Value": [
-            this.exportRecord['thuc_hien_thang'] || 0,
-            this.exportRecord['thuc_hien_thang_truoc'] || 0,
-            this.exportRecord['luy_ke'] || 0,
-            this.exportRecord['ke_hoach_nam'] || 0,
-            this.exportRecord['thuc_hien_thang_cung_ky'] || 0,
-            this.exportRecord['luy_ke_cung_ky'] || 0,
-            this.exportRecord['thuc_hien_thang_so_voi_cung_ky'] || 0,
-            this.exportRecord['thuc_hien_so_voi_ke_hoach'] || 0,
-            this.exportRecord['luy_ke_so_voi_cung_ky'] || 0,
-            this.exportRecord['luy_ke_so_voi_ke_hoach'] || 0,
-          ]
-        },
-        {
-          "Indicator": this.importIndicator,
-          "Value": [
-            this.importRecord['thuc_hien_thang'] || 0,
-            this.importRecord['thuc_hien_thang_truoc'] || 0,
-            this.importRecord['luy_ke'] || 0,
-            this.importRecord['ke_hoach_nam'] || 0,
-            this.importRecord['thuc_hien_thang_cung_ky'] || 0,
-            this.importRecord['luy_ke_cung_ky'] || 0,
-            this.importRecord['thuc_hien_thang_so_voi_cung_ky'] || 0,
-            this.importRecord['thuc_hien_so_voi_ke_hoach'] || 0,
-            this.importRecord['luy_ke_so_voi_cung_ky'] || 0,
-            this.importRecord['luy_ke_so_voi_ke_hoach'] || 0,
-          ]
-        }
-        ]
+        "Data": input
       },
       "access_token": this.access_token
     }
     this.sctService.PostLrisReportData(payload).subscribe((result) => {
       if (result) {
+        this.getCurrentMonthData();
         this.logger.msgSuccess("Gửi dữ liệu lên phần mềm Kinh tế xã hội thành công!");
       } else {
         this.logger.msgError("Lỗi khi gửi dữ liệu tới phần mềm Kinh tế xã hội");
